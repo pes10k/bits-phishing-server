cf6d50e169d22324e423a95d013eb89c60b90167
Peter Snyder <snyderp@gmail.com>
2014-10-05 16:56:26 -0500

    add group matching tool

--- scripts/participants.py
+++ scripts/participants.py
@@ -11,7 +11,6 @@ import sys
 import pymongo
 import argparse
 
-
 parser = argparse.ArgumentParser(description=sys.modules[__name__].__doc__)
 parser.add_argument("--days", "-d", type=int, default=2,
                     help="The number of days used as a threshold for " +
@@ -26,6 +25,9 @@ parser.add_argument("--inemails", "-e", action="store_true",
 parser.add_argument("--outemails", "-o", action="store_true",
                     help="Prints the email address of all participants " +
                          "that have not reported since the theshold date.")
+parser.add_argument("--group", "-g", default=None,
+                    help="If provided, lists all install ids that are active " +
+                         "and assigned to a given group.")
 args = parser.parse_args()
 
 
@@ -39,41 +41,37 @@ db = connection[config.mongo_database]
 
 threshold = datetime.datetime.now() - datetime.timedelta(days=args.days)
 
+def is_active(row):
+    checkins = row["checkins"]
+    if len(checkins) == 0:
+        return False
+    latest_checkin = checkins[-1]['time']
+    return latest_checkin >= threshold
+
 if args.active:
     num_active = 0
     num_inactive = 0
 
     for row in db.installs.find({}, {"checkins": 1}):
-        checkins = row["checkins"]
-        if len(checkins) == 0:
-            num_inactive += 1
-            continue
-
-        latest_checkin = checkins[-1]['time']
-        if latest_checkin >= threshold:
+        if is_active(row):
             num_active += 1
         else:
             num_inactive += 1
+
     print "# Active:   {}".format(num_active)
     print "# Inactive: {}".format(num_inactive)
 
 if args.inemails:
     for row in db.emails.find({}, {"checkins": 1}):
-        checkins = row["checkins"]
-        if len(checkins) == 0:
-            continue
-
-        latest_checkin = checkins[-1]['time']
-        if latest_checkin >= threshold:
-            print "{} {}".format(row["_id"], latest_checkin)
+        if is_active(row):
+            print row["_id"]
 
 if args.outemails:
     for row in db.emails.find({}, {"checkins": 1}):
-        checkins = row["checkins"]
-        if len(checkins) == 0:
+        if not is_active(row):
             print row["_id"]
-            continue
 
-        latest_checkin = checkins[-1]['time']
-        if latest_checkin < threshold:
+if args.group:
+    for row in db.installs.find({"group": args.group}, {"checkins": 1}):
+        if is_active(row):
             print row["_id"]
