450a1f1c4526e2fc7a565b4cfc20b54e62b4b5ed
Peter Snyder <snyderp@gmail.com>
2014-10-17 00:01:58 -0500

    add 'how many misses' tool

--- scripts/participants.py
+++ scripts/participants.py
@@ -27,7 +27,11 @@ parser.add_argument("--outemails", "-o", action="store_true",
                          "that have not reported since the theshold date.")
 parser.add_argument("--group", "-g", default=None,
                     help="If provided, lists all install ids that are active " +
-                         "and assigned to a given group.")
+                         "and ,assigned to a given group.")
+parser.add_argument("--misses", "-m", action="store_true",
+                    help="Print out a list of accounts that have participated" +
+                         "but which have a gap of greater than {--days} " +
+                         "in their reporting.")
 args = parser.parse_args()
 
 
@@ -39,10 +43,11 @@ m = config.mongo_params
 connection = pymongo.MongoClient(m['host'], m['port'])
 db = connection[config.mongo_database]
 
-threshold = datetime.datetime.now() - datetime.timedelta(days=args.days)
+threshold_diff = datetime.timedelta(days=args.days)
+threshold = datetime.datetime.now() - threshold_diff
 
-def is_active(row):
-    checkins = row["checkins"]
+def is_active(record):
+    checkins = record["checkins"]
     if len(checkins) == 0:
         return False
     latest_checkin = checkins[-1]['time']
@@ -74,4 +79,19 @@ if args.outemails:
 if args.group:
     for row in db.installs.find({"group": args.group}, {"checkins": 1}):
         if is_active(row):
+            print row["_id"]
+
+if args.misses:
+    for row in db.emails.find({}):
+        if "checkins" not in row:
+            continue
+        prev_time = None
+        adjoining_dates = []
+        for checkin in row["checkins"]:
+            time = checkin['time']
+            if prev_time:
+                adjoining_dates.append((prev_time, time))
+            prev_time = time
+        max_time = max([p - c for p, c in adjoining_dates])
+        if max_time > threshold_diff:
             print row["_id"]
